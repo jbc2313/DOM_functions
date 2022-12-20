a2d83296c1f75f8ec94e29c61481a4765ee70b48
jb corbin <101614021+jbc2313@users.noreply.github.com>
2022-12-20 15:11:25 -0600

    added query selector to toast

--- src/cpy.ts
+++ src/cpy.ts
@@ -1,9 +1,12 @@
 export function copy(event: any) {
     //console.log('event target ==', event);
+    const toast = document.querySelector("#toaster");
+    console.log('toast el =', toast);
     console.log('you clicked the mouse!')
     const text: string = event.target.innerText;
     console.log('text saved =', text);
     navigator.clipboard.writeText(text)
+    
 
 
 }
