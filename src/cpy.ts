94573d199debcfe1feeea6ff4be3409f7abcd64a
jb corbin <101614021+jbc2313@users.noreply.github.com>
2022-12-20 01:22:45 -0600

    added navigator.clipboard.writetext

--- src/cpy.ts
+++ src/cpy.ts
@@ -3,7 +3,7 @@ export function copy(event: any) {
     console.log('you clicked the mouse!')
     const text: string = event.target.innerText;
     console.log('text saved =', text);
-
+    navigator.clipboard.writeText(text)
 
 
 }
