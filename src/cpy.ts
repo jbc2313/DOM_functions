d3d7f6195f5afdba2ad6a611c34c5267d309676c
jb corbin <101614021+jbc2313@users.noreply.github.com>
2022-12-20 01:12:45 -0600

    saving inner text

--- src/cpy.ts
+++ src/cpy.ts
@@ -1,7 +1,8 @@
 export function copy(event: any) {
-    console.log('event target ==', event.target);
+    //console.log('event target ==', event);
     console.log('you clicked the mouse!')
-
+    const text: string = event.target.innerText;
+    console.log('text saved =', text);
 
 
 
