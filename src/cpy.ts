1b7628aeae08ee17b409ab35d981266320e00fa0
jb corbin <101614021+jbc2313@users.noreply.github.com>
2022-12-20 22:58:04 -0600

    added settimeout to toast

--- src/cpy.ts
+++ src/cpy.ts
@@ -1,12 +1,20 @@
+
+// need to create a settimeout to add a class to toast to make it visible for 4 seconds.
+// then remove the class after the timeout.
+
 export function copy(event: any) {
     //console.log('event target ==', event);
+
     const toast = document.querySelector("#toaster");
     console.log('toast el =', toast);
     console.log('you clicked the mouse!')
     const text: string = event.target.innerText;
     console.log('text saved =', text);
     navigator.clipboard.writeText(text)
-    
+    toast?.classList.add('vis');
+    setTimeout(() => {
+        toast?.classList.remove('vis'); 
+    }, 3000)
 
 
 }
